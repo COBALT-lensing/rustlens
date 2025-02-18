8bb7348a75084d1878444389514f924312316559
Adam McMaster <me@adam-mcmaster.com>
2025-02-18 21:56:07 +0000

    Take l as a list/vector

--- src/lib.rs
+++ src/lib.rs
@@ -19,7 +19,7 @@ fn ld_linear(r: f64) -> f64 {
 }
 
 #[pyfunction]
-pub fn integrated_witt_mao_magnification(l: f64, re: f64, rstar: f64) -> PyResult<f64> {
+pub fn integrated_witt_mao_magnification(l: Vec<f64>, re: f64, rstar: f64) -> PyResult<Vec<f64>> {
     let b_int = integrate(
         |r: f64| -> f64 { 2.0 * f64::consts::PI * r * ld_linear(r) },
         0.0,
@@ -27,87 +27,99 @@ pub fn integrated_witt_mao_magnification(l: f64, re: f64, rstar: f64) -> PyResul
         1e-16,
     )
     .integral;
-    let umag = witt_mao_magnification(l, re, rstar)?;
-    let radial_witt_mao_magnification = |r: f64| -> f64 {
-        if r < 0.0 {
-            return 0.0;
-        }
-        return umag;
-    };
-    let mag_deriv = |r: f64| -> f64 {
-        if r < 0.0 {
-            return 0.0;
-        }
-        return sderivative(
-            &|x: f64| -> f64 { radial_witt_mao_magnification(x) },
-            r,
-            None,
-        );
-    };
-    let mag_int = integrate(
-        |r: f64| -> f64 {
-            2.0 * f64::consts::PI
-                * r
-                * (radial_witt_mao_magnification(r) + r / 2.0 * mag_deriv(r))
-                * ld_linear(r)
-        },
-        0.0,
-        1.0,
-        1e-16,
-    )
-    .integral;
-    return Ok(mag_int / b_int);
+    let mut res = Vec::new();
+    for umag in witt_mao_magnification(l, re, rstar)? {
+        let radial_witt_mao_magnification = |r: f64| -> f64 {
+            if r < 0.0 {
+                return 0.0;
+            }
+            return umag;
+        };
+        let mag_deriv = |r: f64| -> f64 {
+            if r < 0.0 {
+                return 0.0;
+            }
+            return sderivative(
+                &|x: f64| -> f64 { radial_witt_mao_magnification(x) },
+                r,
+                None,
+            );
+        };
+        let mag_int = integrate(
+            |r: f64| -> f64 {
+                2.0 * f64::consts::PI
+                    * r
+                    * (radial_witt_mao_magnification(r) + r / 2.0 * mag_deriv(r))
+                    * ld_linear(r)
+            },
+            0.0,
+            1.0,
+            1e-16,
+        )
+        .integral;
+        res.push(mag_int / b_int);
+    }
+    return Ok(res);
 }
 
 #[pyfunction]
-pub fn witt_mao_magnification(l: f64, re: f64, rstar: f64) -> PyResult<f64> {
+pub fn witt_mao_magnification(l: Vec<f64>, re: f64, rstar: f64) -> PyResult<Vec<f64>> {
     let rstar_scaled: f64 = rstar / re;
     let rstar_scaled2: f64 = rstar_scaled.powi(2);
-    let l_scaled: f64 = l * rstar_scaled;
 
-    let l_r_diff: f64 = l_scaled - rstar_scaled;
-    let l_r_sum: f64 = l_scaled + rstar_scaled;
+    let mut res: Vec<f64> = Vec::new();
 
-    if (l_r_diff).abs() < 1e-5 {
-        return Ok(((2.0 / rstar_scaled)
-            + ((1.0 + rstar_scaled2) / rstar_scaled2)
-                * ((f64::consts::PI / 2.0)
-                    + ((rstar_scaled2 - 1.0) / (rstar_scaled2 + 1.0)).asin()))
-            / f64::consts::PI);
+    for _l in l.iter() {
+        let l_scaled: f64 = _l * rstar_scaled;
+
+        let l_r_diff: f64 = l_scaled - rstar_scaled;
+        let l_r_sum: f64 = l_scaled + rstar_scaled;
+
+        if l_r_diff.abs() < 1e-5 {
+            res.push(
+                ((2.0 / rstar_scaled)
+                    + ((1.0 + rstar_scaled2) / rstar_scaled2)
+                        * ((f64::consts::PI / 2.0)
+                            + ((rstar_scaled2 - 1.0) / (rstar_scaled2 + 1.0)).asin()))
+                    / f64::consts::PI,
+            );
+            continue;
+        }
+
+        let kernel1: f64 = (l_r_diff).powi(2);
+        let kernel2: f64 = (4.0 + kernel1).sqrt();
+
+        let elliptic_n: f64 = 4.0 * rstar_scaled * l_scaled / (l_r_sum).powi(2);
+
+        let elliptic_k: f64 = (4.0 * elliptic_n).sqrt() / kernel2;
+        let elliptic_m: f64 = elliptic_k.powi(2);
+
+        let first_term: f64 = (l_r_sum * kernel2) / (2.0 * rstar_scaled2);
+        let second_term: f64 = l_r_diff * (4.0 + (0.5 * (l_scaled.powi(2) - rstar_scaled2)))
+            / (kernel2 * rstar_scaled2);
+        let third_term: f64 =
+            2.0 * kernel1 * (1.0 + rstar_scaled2) / (rstar_scaled2 * l_r_sum * kernel2);
+
+        let ellip1: f64 = match ellip::ellipe(elliptic_m) {
+            Ok(v) => v,
+            Err(e) => return Err(PyRuntimeError::new_err(e)),
+        };
+        let ellip2: f64 = match ellip::ellipk(elliptic_m) {
+            Ok(v) => v,
+            Err(e) => return Err(PyRuntimeError::new_err(e)),
+        };
+        let ellip3: f64 = match ellippi(elliptic_n, elliptic_m) {
+            Ok(v) => v,
+            Err(e) => return Err(PyRuntimeError::new_err(e)),
+        };
+
+        let kernel3: f64 = ellip1 * first_term - ellip2 * second_term + ellip3 * third_term;
+
+        let pos: f64 = (kernel3 + f64::consts::PI) / (2.0 * f64::consts::PI);
+        let neg: f64 = (kernel3 - f64::consts::PI) / (2.0 * f64::consts::PI);
+        res.push(pos + neg);
     }
-
-    let kernel1: f64 = (l_r_diff).powi(2);
-    let kernel2: f64 = (4.0 + kernel1).sqrt();
-
-    let elliptic_n: f64 = 4.0 * rstar_scaled * l_scaled / (l_r_sum).powi(2);
-
-    let elliptic_k: f64 = (4.0 * elliptic_n).sqrt() / kernel2;
-    let elliptic_m: f64 = elliptic_k.powi(2);
-
-    let first_term: f64 = (l_r_sum * kernel2) / (2.0 * rstar_scaled2);
-    let second_term: f64 =
-        (l_r_diff) * (4.0 + (0.5 * (l_scaled.powi(2) - rstar_scaled2))) / (kernel2 * rstar_scaled2);
-    let third_term: f64 =
-        2.0 * kernel1 * (1.0 + rstar_scaled2) / (rstar_scaled2 * l_r_sum * kernel2);
-
-    let ellip1: f64 = match ellip::ellipe(elliptic_m) {
-        Ok(v) => v,
-        Err(e) => return Err(PyRuntimeError::new_err(e)),
-    };
-    let ellip2: f64 = match ellip::ellipk(elliptic_m) {
-        Ok(v) => v,
-        Err(e) => return Err(PyRuntimeError::new_err(e)),
-    };
-    let ellip3: f64 = match ellippi(elliptic_n, elliptic_m) {
-        Ok(v) => v,
-        Err(e) => return Err(PyRuntimeError::new_err(e)),
-    };
-
-    let kernel3: f64 = ellip1 * first_term - ellip2 * second_term + ellip3 * third_term;
-
-    let pos: f64 = (kernel3 + f64::consts::PI) / (2.0 * f64::consts::PI);
-    let neg: f64 = (kernel3 - f64::consts::PI) / (2.0 * f64::consts::PI);
-    return Ok(pos + neg);
+    return Ok(res);
 }
 
 /// A Python module implemented in Rust.
